c93866cdc15b7720a4331cc041c0511536ea4db2
George Adams <georgeadams@DRs-MacBook-Pro.local>
2019-10-23 16:46:50 -0500

    added login form

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link , Switch} from "react-router-dom";
+import Login from "./Login";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">Boiler Plate</div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+       <Switch>
+       <Route exact path="/" component={Login} />
+       </Switch>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
@@ -21,3 +22,29 @@ export default connect(
   mapStateToProps,
   {}
 )(App);
+
+
+
+// import React from "react";
+// import "./App.css";
+// import { connect } from "react-redux";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div className="App">Boiler Plate</div>
+//       </Router>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {};
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   {}
+// )(App);
--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
     <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
