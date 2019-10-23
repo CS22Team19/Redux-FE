e4345a786c49f5813901011ee1e3aa22264777af
Joshua Wood <JerichoWall@apu.edu>
2019-10-23 14:57:17 -0700

    Player move boiler plate added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+export const PLAYER_MOVE = "PLAYER_MOVE";
+export const PLAYER_MOVE_SUCCESS = "PLAYER_MOVE_SUCCESS";
+export const PLAYER_MOVE_FAIL = "PLAYER_MOVE_FAIL";
+
+export const playerMove = () => dispatch => {
+  dispatch({ type: PLAYER_MOVE });
+  axios;
+  try {
+  } catch {}
+};
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,16 @@
-import {} from "../actions";
+import { PLAYER_MOVE, PLAYER_MOVE_SUCCESS, PLAYER_MOVE_FAIL } from "../actions";
 
 const initialState = {};
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case PLAYER_MOVE:
+      return {};
+    case PLAYER_MOVE_SUCCESS:
+      return {};
+    case PLAYER_MOVE_FAIL:
+      return {};
+
     default:
       return state;
   }
