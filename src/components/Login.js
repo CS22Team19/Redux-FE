1131c4e6f9b5d51635baa9ec3c42243abc5c32f7
Joshua Wood <JerichoWall@apu.edu>
2019-10-24 11:12:57 -0700

    Updated login data shape

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,89 +1,76 @@
-// If set up properly, your backend will create the user instance, salt the password using 
-// BCrypt, and then return an object with a user key and a jwt key. This object is the important 
-// part to Auth. You’ll see it later in this tutorial, but we will essentially take the user object 
+// If set up properly, your backend will create the user instance, salt the password using
+// BCrypt, and then return an object with a user key and a jwt key. This object is the important
+// part to Auth. You’ll see it later in this tutorial, but we will essentially take the user object
 // and save it to your Redux store, then take the token associated with the user and save it to localStorage.
 
-
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import {connect} from 'react-redux';
 // import {userLoginFetch} from '../actions';
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import "./Login.css";
 
 class Login extends Component {
-    state = {
-        email: "",
-        password: "",
-       
-      }
-  
+  state = {
+    username: "",
+    password: ""
+  };
 
-      handleChange = event => {
-        this.setState({
-          [event.target.name]: event.target.value
-        });
-      }
-    
-      handleSubmit = event => {
-        event.preventDefault()
-        // this.props.userLoginFetch(this.state)
-      }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // this.props.userLoginFetch(this.state)
+  };
 
   render() {
-  return (
-    <div className="Login">
-      <form onSubmit={this.handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            name="email"
-            type="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            value={this.state.password}
-            onChange={this.handleChange}
-            type="password"
-            name='password'
-          />
-        </FormGroup>
-        <Button block bsSize="large"  type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  )
-}
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Username</FormLabel>
+            <FormControl
+              autoFocus
+              name="username"
+              type="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+              name="password"
+            />
+          </FormGroup>
+          <Button block bsSize="large" type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default Login
+export default Login;
 
 // const mapDispatchToProps = dispatch => ({
 //     userLoginFetch: userInfo => dispatch(userLoginFetch(userInfo))
 //   })
-  
+
 //   export default connect(null, mapDispatchToProps)(Login);
 
-
-
-
-
-
-
-
-
-// If set up properly, your backend will create the user instance, salt the password using 
-// BCrypt, and then return an object with a user key and a jwt key. This object is the important 
-// part to Auth. You’ll see it later in this tutorial, but we will essentially take the user object 
+// If set up properly, your backend will create the user instance, salt the password using
+// BCrypt, and then return an object with a user key and a jwt key. This object is the important
+// part to Auth. You’ll see it later in this tutorial, but we will essentially take the user object
 // and save it to your Redux store, then take the token associated with the user and save it to localStorage.
-
 
 // import React, { useState } from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
