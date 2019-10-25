41515f256a89a8164862dbec3624b2135a464628
Joshua Wood <JerichoWall@apu.edu>
2019-10-24 17:59:26 -0700

    Boiler plate for hud

---
+++ src/components/Hud.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Hud extends React.Component {
+  state = {};
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default connect(
+  null,
+  {}
+)(Hud);
